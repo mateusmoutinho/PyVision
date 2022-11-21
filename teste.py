2d918b09a022048c1a010cafec49ffb623d056ad
mateusmoutinho <mateusmoutinho01@gmail.com>
2022-11-21 07:11:42 -0300

    testing acess

--- teste.py
+++ teste.py
@@ -5,22 +5,14 @@ def soma(x,y):
     soma.breakpoint(locals())
     return x+y
 
-def format_list(elementos):
+def format_list():
     estagio = 'entrada'
-    format_list = Stack(ignore=['elementos'])
-    
-    format_list.render(locals())
-    for l in elementos:
-        estagio = 'loop'
-        r = soma(l['x'],l['y'])
-        format_list.breakpoint(locals())
+    l = locals
 
+
+    r = 20 
+
+    print(l())
 MainStack.filetipe = 'json'
-format_list([
-
-    {'x':1,'y':2},
-    {'x':3,'y':4},
-    {'x':5,'y':6},
-
- ])
+format_list()
 
