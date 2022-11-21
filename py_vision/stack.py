45b35ec162ad7a72c2fe20d1cbc9f06d65d44abd
mateusmoutinho <mateusmoutinho01@gmail.com>
2022-11-21 04:46:01 -0300

    att

--- py_vision/stack.py
+++ py_vision/stack.py
@@ -1,2 +1,27 @@
+import yaml 
+import json 
+import os
+import time
 
 
+class Stack:
+    stack = {}
+    name = "stack"
+    filetipe = "yml"
+    production = True
+    
+    
+    
+    @staticmethod
+    def render() -> None:
+        if Stack.filetipe == 'yml':
+            Stack.stack = yaml.load(open(f'{Stack.name}.yml'),Loader=yaml.FullLoader)
+        
+        elif Stack.filetipe == 'json':
+            Stack.stack = json.load(open(f'{Stack.name}.json'))
+        
+        else:
+            raise Exception('Filetipe not supported')
+    
+    
+    
