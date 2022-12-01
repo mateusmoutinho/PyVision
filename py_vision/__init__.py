ef30fc2816e7381024c3685bbef1d3441406d5bb
Mateus Moutinho Queiroz <73533622+Mateusominjus@users.noreply.github.com>
2022-12-01 15:38:15 -0300

    att

--- py_vision/__init__.py
+++ py_vision/__init__.py
@@ -3,4 +3,4 @@ from inspect import currentframe
 from py_vision.main_stack import MainStack
 from py_vision.sub_stack import SubStack
 
-from py_vision.view.exec_view import main as view_stack
+from py_vision.view.view import main as view_stack
