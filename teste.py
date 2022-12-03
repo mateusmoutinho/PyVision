f6b04058d24e691cf28fada3093dd6859ed49f09
Mateus Moutinho Queiroz <73533622+Mateusominjus@users.noreply.github.com>
2022-12-03 18:00:17 -0300

    enable mecanic

--- teste.py
+++ teste.py
@@ -4,7 +4,7 @@ from py_vision import *
 def imprimi(substack:SubStack,valor):
     simprimi = substack.sub_stack(currentframe())
     print(valor)
-    simprimi.plot()
+   
     simprimi.end()
 
 def main(stack:MainStack):
@@ -12,7 +12,7 @@ def main(stack:MainStack):
     for x in range(10):
         imprimi(smain,x)
     r = 20
-    smain.plot()
+    smain.end()
 
 
 
